0bb8cc9e4c7f08ccfe7b26c06e00f279d8628856
Dobromir Kirov <kirov0407@gmail.com>
2023-10-16 13:53:12 +0300

    login / logout flow, remember user emial

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,79 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { Input } from "./components";
 import axios from "axios";
+import { User } from "./types";
 
 function App() {
-  const handleLoginReq = () => {
-    axios.post("/login", {
-      email: "Fred",
-      password: "Flintstone",
+  const isUser = sessionStorage.getItem("is-authenticated") === "true";
+  const isSaved = localStorage.getItem("user_email");
+
+  const [user, setUser] = useState<User>({
+    email: "",
+    isAuth: null,
+  });
+
+  const [saved, setSaved] = useState(!!isSaved);
+
+  const handleLoginReq = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const email: string = e.currentTarget.email.value;
+    const password: string = e.currentTarget.password.value;
+
+    if (saved) {
+      localStorage.setItem("user_email", email);
+    } else {
+      localStorage.removeItem("user_email");
+    }
+
+    axios
+      .post("/login", {
+        email,
+        password,
+      })
+      .then(() => setUser({ email: email, isAuth: true }))
+      .catch(() => console.log("Error"));
+  };
+
+  const handleLogout = () => {
+    sessionStorage.setItem("is-authenticated", "false");
+    setUser({
+      email: isSaved ? isSaved : "",
+      isAuth: false,
     });
   };
 
+  useEffect(() => {
+    setUser({ email: isSaved ? isSaved : "", isAuth: isUser });
+  }, []);
+
   return (
     <div className="App">
-      <form>
-        <Input type="email" />
-        <Input type="password" />
-        <Input type="checkbox" />
-      </form>
-      <button onClick={handleLoginReq}>Login</button>
+      {user.isAuth ? (
+        <>
+          <p>Hi, {user.email}</p>
+          <button onClick={handleLogout}>Logout</button>
+        </>
+      ) : (
+        <form onSubmit={(e) => handleLoginReq(e)}>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            value={user.email}
+            onChange={(e) => setUser({ ...user, email: e.target.value })}
+          />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+          <label htmlFor="remember">Remember me</label>
+          <input
+            type="checkbox"
+            id="remember"
+            checked={saved}
+            onChange={() => setSaved(!saved)}
+          />
+          <button type="submit">Login</button>
+        </form>
+      )}
     </div>
   );
 }
